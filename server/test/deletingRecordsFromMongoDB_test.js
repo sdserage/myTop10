4093e4eba0600f2b43aadc0228565bd897b3b9b9
sdserage <xttocsurge51@gmail.com>
2018-11-14 10:14:08 -0700

    Renamed more tests again (still trying to figure out how test file names should look), completed the deleting records from MongoDB test

---
+++ server/test/deletingRecordsFromMongoDB_test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const { User } = require('../mongodbModels/user');
+const uuidv4 = require('../util/uuidv4');
+
+describe('Deleting records', () => {
+  let newUser;
+
+  beforeEach(done => {
+    newUser = new User({
+      auth0ID: uuidv4(),
+      firstName: 'Josuke',
+      lastName: 'Higashikata',
+      userName: 'shiningDiamond',
+      email: 'higashikatajosuke@jojo.com',
+      lists: [],
+    });
+
+    newUser.save().then(() => {
+      done();
+    });
+  });
+
+  it('Deletes one record from the database', done => {
+    User.findOneAndDelete({name: 'Josuke'}).then(result => {
+      User.findOne({name: 'Josuke'}).then(result => {
+        assert(result === null);
+        done();
+      });
+    });
+  });
+});
