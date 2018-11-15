f3d6b92a370cf1a9e7325b99feeec7959fbde94a
sdserage <xttocsurge51@gmail.com>
2018-11-15 16:40:16 -0700

    Added .next to .gitignore

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,19 +1,26 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
   }
 
-  render () {
+  render() {
     return (
       <html>
         <Head>
-          {/* <title>My Top 10</title> */}
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            hid="description"
+            name="description"
+            content="Native-like Page Transitions with React and NextJS, A Travel App"
+          />
           {this.props.styleTags}
         </Head>
         <body>
@@ -21,6 +28,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
