d4eea9f7e2b6ebeb44afee5c735b8950db8792fe
sdserage <xttocsurge51@gmail.com>
2018-11-20 21:40:40 -0700

    Created simple form

--- client/pages/my-lists/new-list.js
+++ client/pages/my-lists/new-list.js
@@ -63,6 +63,7 @@ const modeMachine = Machine(
 );
 
 function reducer(state, { type, payload }) {
+  console.log(state, type, payload)
   switch (type) {
     case 'UPDATE_TITLE':
       return {...state, title: payload};
@@ -79,8 +80,6 @@ function reducer(state, { type, payload }) {
   }
 }
 
-
-
 export default function NewList(props) {
   const [ state, updateState ] = useState({
     title: '',
@@ -89,16 +88,65 @@ export default function NewList(props) {
     size: 10,
     items: [],
   });
+  const [subCategory, updateSubCategory] = useState('');
+  const [item, updateItem] = useState({});
 
   function dispatch(type, payload) {
-    return reducer(state, {type, payload});
+    updateState(reducer(state, {type, payload}));
   }
   
+  console.log("Current state: ", state);
   return (
     <PageWrapper>
       <h1>New List</h1>
       <form>
-        
+        <input
+          type="text"
+          placeholder="Title"
+          value={state.title}
+          onChange={e => dispatch('UPDATE_TITLE', e.target.value)}
+        />
+        <select value={state.category} onChange={e => dispatch('UPDATE_CATEGORY', e.target.value)}>
+          <option value="" disabled>Choose a category</option>
+          <option value="Sports">Sports</option>
+          <option value="TV Shows">TV Shows</option>
+          <option value="Literature">Literature</option>
+          <option value="Health">Health</option>
+          <option value="Gaming">Gaming</option>
+          <option value="Animals">Animals</option>
+          <option value="Music">Music</option>
+          <option value="Food">Food</option>
+          <option value="Hobbies">Hobbies</option>
+          <option value="Nature">Nature</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Sci-Fi">Sci-Fi</option>
+          <option value="Technology">Technology</option>
+          <option value="History">History</option>
+          <option value="Science">Science</option>
+          <option value="Outdoor Recreation">Outdoor Recreation</option>
+        </select>
+
+        <ul>
+          {state.subCategories.map((subCategory, index) => <li key={index}>{subCategory}</li>)}
+          <input type="text" value={subCategory} placeholder="Add a sub category" onChange={e => updateSubCategory(e.target.value)} />
+          <button
+            onClick={e => {
+              e.preventDefault();
+              dispatch('UPDATE_SUBCATEGORIES', [...state.subCategories, subCategory]);
+              updateSubCategory('');
+            }}
+          >
+            Save Sub Category
+          </button>
+        </ul>
+
+        <input
+          type='text'
+          pattern="\d*"
+          value={state.size}
+          onChange={e => dispatch('UPDATE_SIZE', Number(e.target.value))}
+        />
+
       </form>
     </PageWrapper>
   );
